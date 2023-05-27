742cd858a86da76ec43a2f2a9398347e3c302800
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-28 01:37:30 +0200

    Added custom gradient main background

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -4,7 +4,7 @@ import Header from "../components/header";
 
 function App() {
   return (
-    <main>
+    <main className="bg-background-gradient">
       <Header />
       <Form />
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-gradient':  "linear-gradient(168.13deg, #F8D696 4.91%, #EA8AF2 43.43%, #A972EE 98.14%)",
+      },
+    },
   },
   plugins: [],
 }
