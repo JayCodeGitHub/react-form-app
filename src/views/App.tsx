702e7065a6209b769bf7659f91062cc6e82a8de6
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-28 02:14:28 +0200

    Added flex and gap to main

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -4,7 +4,7 @@ import Header from "../components/header";
 
 function App() {
   return (
-    <main className="absolute w-full min-h-screen bg-background-gradient py-12 md:py-28">
+    <main className="absolute w-full min-h-screen bg-background-gradient flex flex-col items-center gap-12 py-12 md:py-28">
       <Header />
       <Form />
     </main>
