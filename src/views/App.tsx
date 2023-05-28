229b24daf639d25618995e284bc132fcca405bc3
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-28 03:30:01 +0200

    Added title with react helmet

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Form from "../components/form";
 import Header from "../components/header";
 
 function App() {
   return (
-    <main className="absolute w-full min-h-screen bg-background-gradient flex flex-col items-center gap-12 py-12 md:py-28">
-      <Header />
-      <Form />
-    </main>
+    <>
+      <Helmet>
+        <title>React Form App</title>
+      </Helmet>
+      <main className="absolute flex flex-col items-center w-full min-h-screen gap-12 py-12 bg-background-gradient md:py-28">
+        <Header />
+        <Form />
+      </main>
+    </>
   );
 }
 
