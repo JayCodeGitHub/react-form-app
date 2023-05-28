d16036d3771b2d1860ae74f4d3858a12b87d252a
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-28 02:07:20 +0200

    Added custom font family and font size for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1 className="text-[#000853] text-4xl text-center">
+      <h1 className="text-[#000853] font-inter text-4xl text-center">
         Marathon <br className="md:hidden" />
         Application Form
       </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontSize: {
+      '4xl': ['36px', '44px'],
+    },
     extend: {
       backgroundImage: {
         'background-gradient':  "linear-gradient(168.13deg, #F8D696 4.91%, #EA8AF2 43.43%, #A972EE 98.14%)",
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
